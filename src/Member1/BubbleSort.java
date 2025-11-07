e92d6f66fd1e5f35e71d36e2a5dab0d5dbf3fd7f
Nishamani <Nishamani@DESKTOP-LMQLOE7>
2025-11-07 14:49:29 +0530

    Add buubble sort class

--- src/Member1/BubbleSort.java
+++ src/Member1/BubbleSort.java
@@ -19,10 +19,6 @@ public class BubbleSort {
         System.out.println("Bubble Sort Steps: " + steps);
         return arr;
     }
-    public static int getSteps() {
-        return steps;
-    }
 }
-
 
     
