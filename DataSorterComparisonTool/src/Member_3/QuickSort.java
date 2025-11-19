fd125558e28618730c40ee0595079cc3b0daf439
Rashindii <rashindihashara4375@gmail.com>
2025-11-19 14:20:33 +0530

    Initial commit of QuickSort project

---
+++ DataSorterComparisonTool/src/Member_3/QuickSort.java
@@ -0,0 +1,36 @@
+package Member3;
+
+public class QuickSort {
+    private static int steps = 0;
+
+    public static void sort(int[] arr, int low, int high) {
+        steps = 0; // Reset before sorting
+        quickSort(arr, low, high);
+    }
+
+    private static void quickSort(int[] arr, int low, int high) {
+        if (low < high) {
+            int pi = partition(arr, low, high);
+            quickSort(arr, low, pi - 1);
+            quickSort(arr, pi + 1, high);
+        }
+    }
+
+    private static int partition(int[] arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            steps++;
+            if (arr[j] < pivot) {
+                i++;
+                int temp = arr[i]; arr[i] = arr[j]; arr[j] = temp;
+            }
+        }
+        int temp = arr[i + 1]; arr[i + 1] = arr[high]; arr[high] = temp;
+        return i + 1;
+    }
+
+    public static int getSteps() {
+        return steps;
+    }
+}
